2a921c3ce496781941694bec03f1517b5843dcb1
ThomasPiard <thomas.piard.etudes@gmail.com>
2020-03-23 15:51:09 +0100

    [Core] Add Twig package

--- src/index.php
+++ src/index.php
@@ -5,6 +5,20 @@ use Monolog\Handler\StreamHandler;
 require_once '../vendor/autoload.php';
 $logger = new Logger('main');
 $logger->pushHandler(new StreamHandler(__DIR__ . '/../log/app.log', Logger::DEBUG));
-print("OK");
+//print("OK");
 
 $logger->info('First message');
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+// initialiser l'environnement de Twig et définir le dossier du cache
+$twig = new \Twig\Environment($loader, [
+'cache' => '../cache',
+]);
+// Affecter les variables du template et appeller le rendu
+echo $twig->render('base.html.twig',
+[
+'title' => 'Essai de Twig',
+'text' => 'Texte inséré dans la page.',
+]
+);
